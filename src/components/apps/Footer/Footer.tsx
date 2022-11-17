2977b92c40019c5c46aa17db9ceb0cb865b1c440
moohka <moohka@hotmail.com>
2022-11-17 14:47:37 -0600

    year on footer changed

--- src/components/apps/Footer/Footer.tsx
+++ src/components/apps/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
+  let year = 2023;
+
   return (
     <footer>
-      <span>Copyright © 2022-2023 Grandorca. All rights reserved.</span>
+      <span>Copyright © 2022-{year} Grandorca. All rights reserved.</span>
     </footer>
   );
 };
